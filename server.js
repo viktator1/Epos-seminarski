05b56785ab5b38767ea42e7d0e7dab30d35341f2
viktator1 <viktor06011999@gmail.com>
2021-01-29 18:43:06 +0100

    poruka

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.put("/azuriranjeKredita",(req,res)=>{
 
 
 
-app.listen("3000",()=>{
-    console.log("aplikacija je pokrenuta na portu 3000");
+app.listen(process.env.PORT || 3000,()=>{
+    console.log(`aplikacija je pokrenuta na portu ${PORT}`);
 });
 
